c683d703d8b5cccae05eabf66810e997444b108e
Josh Osborne <joshosbrn@gmail.com>
2022-08-18 14:31:25 -0400

    initial commit

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+const marquee = document.getElementById("marquee");
+  
+animate(marquee);
+  
+function animate(element) {
+    let elementWidth = element.offsetWidth;
+    let parentWidth = element.parentElement.offsetWidth;
+    let flag = 0;
+  
+    setInterval(() => {
+        element.style.marginLeft = --flag + "px";
+  
+        if (elementWidth == -flag) {
+            flag = parentWidth;
+        }
+    }, 10);
+}
